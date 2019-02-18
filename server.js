1b366af3381251fcc2b840c22815b764a58498de
Venkateswararao Lankalapally <vlankala@callidussoftware.com>
2019-02-18 15:22:50 +0530

    Heroku server script added

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname + '/'));
+app.use("*", function (req, resp) {
+    resp.sendFile(__dirname + '/index.html');
+});
+
+var listener = server.listen(process.env.PORT || 5000, function () {
+    console.log('Listening on port ' + listener.address().port); //Listening on port 5000
+});
